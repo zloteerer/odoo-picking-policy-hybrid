90f52ddb2c8123f4f741f075643840f07ea85649
Evan Delagnes <evan.delagnes@gmail.com>
2026-01-06 11:00:45 +0100

    fix: edit description and links

--- __manifest__.py
+++ __manifest__.py
@@ -3,7 +3,7 @@
     'name': "Picking Policy Hybrid - Pack Direct Ship One",
     'summary': 'Pack direct, ship complete picking policy',
     'description': """
-Pack Direct Ship One Policy
+**Tested with Odoo 15 only**
 ===========================
 
 Adds a new picking policy: 'pack_direct_ship_one'
@@ -14,7 +14,7 @@ Adds a new picking policy: 'pack_direct_ship_one'
 Reduces partial customer deliveries while maintaining internal warehouse flexibility.
 """,
     'author': "zloteerer",
-    'website': "github.com/zloteerer",
+    'website': "https://github.com/zloteerer/odoo-picking-policy-hybrid",
     'category': 'Operations/Inventory',
     'version': '15.0.1.0.0',
     'depends': ['sale_stock', 'stock', 'base'],
